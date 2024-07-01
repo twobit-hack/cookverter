81f5a229e24302f0a32e1a99a163f0b222bdf280
parallel-prospecting <126733204+parallel-prospecting@users.noreply.github.com>
2024-07-01 18:34:43 -0500

    Major Restructure
    - Restructured and rewrote program for readability and best practices
    
    - Instead of single step conversions, conversions now convert to cups as a central unit then convert out to the destination unit. This removes the need for nested functions and cut down on lines by half.
    
    - Formatting with .toFixed(2) is not working with the restructured program, so it is removed until I figure it out. Previously, it called the entire program into finalAmount.toFixed(2), but that caused the program to run again in order to format the result.

--- bakingconverter.js
+++ bakingconverter.js
@@ -1,242 +1,114 @@
-    /*
+  /*
     This program converts different units of measurement used in baking
     I plan to incorporate this program into an HTML form
 
     startAmount must be a number
-    startUnit and endUnit must be 'tsp', 'tbsp', 'pints', 'quarts', 'gallons', 'fl oz', or 'mL'
+    startUnit and endUnit must be 'tsp', 'tbsp', 'cups', 'pints', 'quarts', 'gallons', 'fl oz', or 'mL'
 
     */
-const bakingConverter = (startAmount, startUnit, endUnit) => {
-    const bakingConversions = (startAmount, startUnit, endUnit) => {
+const bakingConverter = (startAmount, startUnit, endUnit) => { 
+    //Cups has the most forks in measurement, so I'm using it as the central unit
+    //Section: cupsTo
+    const cupsToTsp = (cups) => {return cups * 48}
+    const cupsToTbsp = (cups) => {return cups * 16}
+    const cupsToPints = (cups) => {return cups / 2}
+    const cupsToQuarts = (cups) => {return cups / 4}
+    const cupsToGallons = (cups) => {return cups / 16}
+    const cupsToOunce = (cups) => {return cups * 8}
+    const cupsToMl = (cups) => {return cups * 236.592}
 
-        //Section: Downward Conversion
-        //To start, I'm defining functions for each single-step conversion, largest to smallest
-        const gallonsToQuarts = (gallons) => {
-            const quarts = gallons * 4;
-            return quarts;
-        }
-        const quartsToPints = (quarts) => {
-            const pints = quarts * 2;
-            return pints;
-        }
-        const pintsToCups = (pints) => {
-            const cups = pints * 2;
-            return cups;
-        }
-        const cupsToOunce = (cups) => {
-            const flOz = cups * 8;
-            return flOz;
-        }
-        const cupsToTbsp = (cups) => {
-            const tbsp = cups * 16;
-            return tbsp;
-        }
-        const tbspToTsp = (tbsp) => {
-            const tsp = tbsp * 3;
-            return tsp;
-        }
-        //I'm including a milliliter option for funsies
-        const ounceToMl = (flOz) => {
-            const ml = flOz * 30;
-            return ml;
-        }
+    //Section: toCups
+    const tspToCups = (tsp) => {return tsp / 48}
+    const tbspToCups = (tbsp) => {return tbsp / 16}
+    const pintsToCups = (pints) => {return pints * 2}
+    const quartsToCups = (quarts) => {return quarts * 4}
+    const gallonsToCups = (gallons) => {return gallons * 16}
+    const ounceToCups = (flOz) => {return flOz / 8}
+    const mlToCups = (mL) => {return mL / 236.592}
 
-        //Section: Upward Conversion
-        //Next, I need to do the same in reverse in case people need to convert upward
-        const mlToOunce = (ml) => {
-            const flOz = ml / 30;
-            return flOz;
-        }
-        const tspToTbsp = (tsp) => {
-            const tbsp = tsp / 3;
-            return tbsp;
-        }
-        const tbspToCups = (tbsp) => {
-            const cups = tbsp / 16;
-            return cups;
-        }
-        const ounceToCups = (flOz) => {
-            const cups = flOz / 8;
-            return cups;
-        }
-        const cupsToPints = (cups) => {
-            const pints = cups / 2;
-            return pints;
-        }
-        const pintsToQuarts = (pints) => {
-            const quarts = pints / 2;
-            return quarts;
-        }
-        const quartsToGallons = (quarts) => {
-            const gallons = quarts / 4;
-            return gallons;
-        }
+    //Section: Conversion Calculator
+    let amount = 0
 
-
-        //All that's left is allowing the program to convert multiple steps at once
-        //Defining the single step functions allows me to shorten the switch-cases by calling the functions in-step
-        
-        
-        //Section: Conversion Calculator
-        switch (startUnit) {
-            case 'tsp':
-                switch (endUnit) {
-                    case 'tbsp':
-                        return tspToTbsp(startAmount);
-                    case 'cups':
-                        return tspToTbsp(tbspToCups(startAmount));
-                    case 'pints':
-                        return tspToTbsp(tbspToCups(cupsToPints(startAmount)));
-                    case 'quarts':
-                        return tspToTbsp(tbspToCups(cupsToPints(pintsToQuarts(startAmount))));
-                    case 'gallons':
-                        return tspToTbsp(tbspToCups(cupsToPints(pintsToQuarts(quartsToGallons(startAmount)))));
-                    case 'fl oz':
-                        return tspToTbsp(tbspToCups(cupsToOunce(startAmount)));
-                    case 'mL':
-                        return tspToTbsp(tbspToCups(cupsToOunce(ounceToMl(startAmount))));
-                    default:
-                        return startAmount;
-                }
-            case 'tbsp':
-                switch (endUnit) {
-                    case 'tsp':
-                        return tbspToTsp(startAmount);
-                    case 'cups':
-                        return tbspToCups(startAmount);
-                    case 'pints':
-                        return tbspToCups(cupsToPints(startAmount));
-                    case 'quarts':
-                        return tbspToCups(cupsToPints(pintsToQuarts(startAmount)));
-                    case 'gallons':
-                        return tbspToCups(cupsToPints(pintsToQuarts(quartsToGallons(startAmount))));
-                    case 'fl oz':
-                        return tbspToCups(cupsToOunce(startAmount));
-                    case 'mL':
-                        return tbspToCups(cupsToOunce(ounceToMl(startAmount)));
-                    default:
-                        return startAmount;
-                }
-            case 'cups':
-                switch (endUnit) {
-                    case 'tsp':
-                        return cupsToTbsp(tbspToTsp(startAmount));
-                    case 'tbsp':
-                        return cupsToTbsp(startAmount);
-                    case 'pints':
-                        return cupsToPints(startAmount);
-                    case 'quarts':
-                        return cupsToPints(pintsToQuarts(startAmount));
-                    case 'gallons':
-                        return cupsToPints(pintsToQuarts(quartsToGallons(startAmount)));
-                    case 'fl oz':
-                        return cupsToOunce(startAmount);
-                    case 'mL':
-                        return cupsToOunce(ounceToMl(startAmount));
-                    default:
-                        return startAmount;
-                }
-            case 'pints':
-                switch (endUnit) {
-                    case 'tsp':
-                        return pintsToCups(cupsToTbsp(tbspToTsp(startAmount)));
-                    case 'tbsp':
-                        return pintsToCups(cupsToTbsp(startAmount));
-                    case 'cups':
-                        return pintsToCups(startAmount);
-                    case 'quarts':
-                        return pintsToQuarts(startAmount);
-                    case 'gallons':
-                        return pintsToQuarts(quartsToGallons(startAmount));
-                    case 'fl oz':
-                        return pintsToCups(cupsToOunce(startAmount));
-                    case 'mL':
-                        return pintsToCups(cupsToOunce(ounceToMl(startAmount)));
-                    default:
-                        return startAmount;
-                }
-            case 'quarts':
-                switch (endUnit) {
-                    case 'tsp':
-                        return quartsToPints(pintsToCups(cupsToTbsp(tbspToTsp(startAmount))));
-                    case 'tbsp':
-                        return quartsToPints(pintsToCups(cupsToTbsp(startAmount)));
-                    case 'cups':
-                        return quartsToPints(pintsToCups(startAmount));
-                    case 'pints':
-                        return quartsToPints(startAmount);
-                    case 'gallons':
-                        return quartsToGallons(startAmount);
-                    case 'fl oz':
-                        return quartsToPints(pintsToCups(cupsToOunce(startAmount)));
-                    case 'mL':
-                        return quartsToPints(pintsToCups(cupsToOunce(ounceToMl(startAmount))));
-                    default:
-                        return startAmount;
-                }
-            case 'gallons':
-                switch (endUnit) {
-                    case 'tsp':
-                        return gallonsToQuarts(quartsToPints(pintsToCups(cupsToTbsp(tbspToTsp(startAmount)))));
-                    case 'tbsp':
-                        return gallonsToQuarts(quartsToPints((pintsToCups(cupsToTbsp(startAmount)))));
-                    case 'cups':
-                        return gallonsToQuarts(quartsToPints(pintsToCups(startAmount)));
-                    case 'pints':
-                        return gallonsToQuarts(quartsToPints(startAmount));
-                    case 'quarts':
-                        return gallonsToQuarts(startAmount);
-                    case 'fl oz':
-                        return gallonsToQuarts(quartsToPints(pintsToCups(cupsToOunce(startAmount))));
-                    case 'mL':
-                        return gallonsToQuarts(quartsToPints(pintsToCups(cupsToOunce(ounceToMl(startAmount)))));
-                    default:
-                        return startAmount;
-                }
-            case 'fl oz':
-                switch (endUnit) {
-                    case 'tsp':
-                        return ounceToCups(cupsToTbsp(tbspToTsp(startAmount)));
-                    case 'tbsp':
-                        return ounceToCups(cupsToTbsp(startAmount));
-                    case 'cups':
-                        return ounceToCups(startAmount);
-                    case 'pints':
-                        return ounceToCups(cupsToPints(startAmount));
-                    case 'quarts':
-                        return ounceToCups(cupsToPints(pintsToQuarts(startAmount)));
-                    case 'gallons':
-                        return(ounceToCups(cupsToPints(pintsToQuarts(quartsToGallons(startAmount)))));
-                    case 'mL':
-                        return (ounceToMl(startAmount));
-                    default:
-                        return startAmount;
-                }
-            case 'mL':
-                switch (endUnit) {
-                    case 'tsp':
-                        return mlToOunce(ounceToCups(cupsToTbsp(tbspToTsp(startAmount))));
-                    case 'tbsp':
-                        return mlToOunce(ounceToCups(cupsToTbsp(startAmount)));
-                    case 'cups':
-                        return mlToOunce(ounceToCups(startAmount));
-                    case 'pints':
-                        return mlToOunce(ounceToCups(cupsToPints(startAmount)));
-                    case 'quarts':
-                        return mlToOunce(ounceToCups(cupsToPints([pintsToQuarts(startAmount)])));
-                    case 'gallons':
-                        return mlToOunce(ounceToCups(cupsToPints(pintsToQuarts(quartsToGallons(startAmount)))));
-                    case 'fl oz':
-                        return mlToOunce(startAmount);
-                    default:
-                        return startAmount;
-                }
-        }
+    switch (startUnit) {
+        case 'tsp':
+            if (endUnit === 'tsp') {return startAmount}
+            amount = tspToCups(startAmount);
+            if (endUnit === 'tbsp') {amount = cupsToTbsp(amount)}
+            if (endUnit === 'cups') {return amount}
+            if (endUnit === 'pints') {amount = cupsToPints(amount)}
+            if (endUnit === 'quarts') {amount = cupsToQuarts(amount)}
+            if (endUnit === 'gallons') {amount = cupsToGallons(amount)}
+            if (endUnit === 'fl oz') {amount = cupsToOunce(amount)}
+            if (endUnit === 'mL') {amount = cupsToMl(amount)}
+        case 'tbsp':
+            if (endUnit === 'tbsp') {return startAmount}
+            amount = tbspToCups(startAmount);
+            if (endUnit === 'tsp') {amount = cupsToTsp(amount)}
+            if (endUnit === 'cups') {return amount}
+            if (endUnit === 'pints') {amount = cupsToPints(amount)}
+            if (endUnit === 'quarts') {amount = cupsToQuarts(amount)}
+            if (endUnit === 'gallons') {amount = cupsToGallons(amount)}
+            if (endUnit === 'fl oz') {amount = cupsToOunce(amount)}
+            if (endUnit === 'mL') {amount = cupsToMl(amount)}
+        case 'cups':
+            if (endUnit === 'cups') {return startAmount}
+            if (endUnit === 'tsp') {amount = cupsToTsp(amount)}
+            if (endUnit === 'tbsp') {amount = cupsToTbsp(amount)}
+            if (endUnit === 'pints') {amount = cupsToPints(amount)}
+            if (endUnit === 'quarts') {amount = cupsToQuarts(amount)}
+            if (endUnit === 'gallons') {amount = cupsToGallons(amount)}
+            if (endUnit === 'fl oz') {amount = cupsToOunce(amount)}
+            if (endUnit === 'mL') {amount = cupsToMl(amount)}
+        case 'pints':
+            if (endUnit === 'pints') {return startAmount}
+            amount = pintsToCups(startAmount);
+            if (endUnit === 'tsp') {amount = cupsToTsp(amount)}
+            if (endUnit === 'tbsp') {amount = cupsToTbsp(amount)}
+            if (endUnit === 'cups') {return amount}
+            if (endUnit === 'quarts') {amount = cupsToQuarts(amount)}
+            if (endUnit === 'gallons') {amount = cupsToGallons(amount)}
+            if (endUnit === 'fl oz') {amount = cupsToOunce(amount)}
+            if (endUnit === 'mL') {amount = cupsToMl(amount)}
+        case 'quarts':
+            if (endUnit === 'quarts') { return startAmount}
+            amount = quartsToCups(startAmount);
+            if (endUnit === 'tsp') {amount = cupsToTsp(amount)}
+            if (endUnit === 'tbsp') {amount = cupsToTbsp(amount)}
+            if (endUnit === 'cups') {return amount}
+            if (endUnit === 'pints') {amount = cupsToPints(amount)}
+            if (endUnit === 'gallons') {amount = cupsToGallons(amount)}
+            if (endUnit === 'fl oz') {amount = cupsToOunce(amount)}
+            if (endUnit === 'mL') {amount = cupsToMl(amount)}
+        case 'gallons':
+            if (endUnit === 'gallons') {return startAmount}
+            amount = gallonsToCups(startAmount);
+            if (endUnit === 'tsp') {amount = cupsToTsp(amount)}
+            if (endUnit === 'tbsp') {amount = cupsToTbsp(amount)}
+            if (endUnit === 'cups') {return amount}
+            if (endUnit === 'pints') {amount = cupsToPints(amount)}
+            if (endUnit === 'quarts') {amount = cupsToQuarts(amount)}
+            if (endUnit === 'fl oz') {amount = cupsToOunce(amount)}
+            if (endUnit === 'mL') {amount = cupsToMl(amount)}
+        case 'fl oz':
+            if (endUnit === 'fl oz') {return startAmount}
+            amount = ounceToCups(startAmount);
+            if (endUnit === 'tsp') {amount = cupsToTsp(amount)}
+            if (endUnit === 'tbsp') {amount = cupsToTbsp(amount)}
+            if (endUnit === 'cups') {return amount}
+            if (endUnit === 'pints') {amount = cupsToPints(amount)}
+            if (endUnit === 'quarts') {amount = cupsToQuarts(amount)}
+            if (endUnit === 'gallons') {amount = cupsToGallons(amount)}
+            if (endUnit === 'mL') {amount = cupsToMl(amount)}
+        case 'mL':
+            if (endUnit === 'mL') {return startAmount}
+            amount = mlToCups(startAmount);
+            if (endUnit === 'tsp') {amount = cupsToTsp(amount)}
+            if (endUnit === 'tbsp') {amount = cupsToTbsp(amount)}
+            if (endUnit === 'cups') {return amount}
+            if (endUnit === 'pints') {amount = cupsToPints(amount)}
+            if (endUnit === 'quarts') {amount = cupsToQuarts(amount)}
+            if (endUnit === 'gallons') {amount = cupsToGallons(amount)}
+            if (endUnit === 'fl oz') {amount = cupsToOunce(amount)}        
     }
-    const finalAmount = bakingConverter(startAmount, startUnit, endUnit);
-    if (finalAmount % 1 === 0) {
-        return finalAmount;
-    } else {
-        return finalAmount.toFixed(2);
-    }
+
 }
